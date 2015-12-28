0c6c502e0f2e7671242b8b35328a7628dd31a8af
lee tebbs <leetebbs@ymail.com>
2015-12-28 21:45:07 +0000

    First commit

---
+++ lists.py
@@ -0,0 +1,22 @@
+#! usr/bin/env python
+
+companions = ['Sarah Jane','leela','Rose Tyler']
+
+print 'the doctor once travelled with',companions[2] + '.'
+
+raw_input("press Enter to Exit")
+
+# A Few ways to maniputlate ;ists 
+
+#companions.append('Tom Jones') #add one new item to the list
+#companions.extend(['Jason williamson','Andrew Fern']) #add multiple new items to the list
+#del companions[0] #delete item from the list
+#companions.pop(0) # delete an item while in use
+
+
+#looping through an array
+
+#for companion in companions: #companion is new var for items in companions lists
+
+#      print 'the doctor traveled with', companion + '.'
+
